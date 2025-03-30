2659284ec9b7145d264fa81fa253434dd8a2bd18
ntimanu <ntihinyukae@gmail.com>
2025-03-30 23:16:06 +0200

    Fix delete and comment a comment by user permitions

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,24 +1,50 @@
-from rest_framework import viewsets, permissions
+from rest_framework import viewsets, permissions, filters
+from rest_framework.response import Response
+from rest_framework.exceptions import PermissionDenied
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-from rest_framework import filters
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission to only allow owners of an object to edit or delete it.
+    """
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request (GET, HEAD, OPTIONS)
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner of the post/comment
+        return obj.author == request.user
 
 class PostViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to perform CRUD operations on Posts.
+    """
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
+        # Assign the authenticated user as the author
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to perform CRUD operations on Comments.
+    """
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
 
     def get_queryset(self):
+        """
+        Ensure users can only retrieve comments belonging to a specific post.
+        """
         return Comment.objects.filter(post_id=self.kwargs['post_pk']).order_by('-created_at')
 
     def perform_create(self, serializer):
+        """
+        Assign the authenticated user as the author of the comment.
+        """
         serializer.save(author=self.request.user)
