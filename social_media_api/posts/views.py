052a5f00adb52fc57bdb7b1ee741d3954566321d
ntimanu <ntihinyukae@gmail.com>
2025-03-30 23:19:31 +0200

    Add permission file that was missing

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -1,24 +1,11 @@
 from rest_framework import viewsets, permissions, filters
-from rest_framework.response import Response
-from rest_framework.exceptions import PermissionDenied
 from .models import Post, Comment
 from .serializers import PostSerializer, CommentSerializer
-
-class IsOwnerOrReadOnly(permissions.BasePermission):
-    """
-    Custom permission to only allow owners of an object to edit or delete it.
-    """
-
-    def has_object_permission(self, request, view, obj):
-        # Read permissions are allowed to any request (GET, HEAD, OPTIONS)
-        if request.method in permissions.SAFE_METHODS:
-            return True
-        # Write permissions are only allowed to the owner of the post/comment
-        return obj.author == request.user
+from .permissions import IsOwnerOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to perform CRUD operations on Posts.
+    API endpoint for users to perform CRUD operations on Posts.
     """
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
@@ -27,12 +14,14 @@ class PostViewSet(viewsets.ModelViewSet):
     search_fields = ['title', 'content']
 
     def perform_create(self, serializer):
-        # Assign the authenticated user as the author
+        """
+        Assign the authenticated user as the author of the post.
+        """
         serializer.save(author=self.request.user)
 
 class CommentViewSet(viewsets.ModelViewSet):
     """
-    API endpoint that allows users to perform CRUD operations on Comments.
+    API endpoint for users to perform CRUD operations on Comments.
     """
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
@@ -40,11 +29,14 @@ class CommentViewSet(viewsets.ModelViewSet):
     def get_queryset(self):
         """
         Ensure users can only retrieve comments belonging to a specific post.
+        This prevents unnecessary exposure of all comments.
         """
-        return Comment.objects.filter(post_id=self.kwargs['post_pk']).order_by('-created_at')
+        post_id = self.kwargs.get('post_pk')  # Ensure filtering by post
+        return Comment.objects.filter(post_id=post_id).order_by('-created_at')
 
     def perform_create(self, serializer):
         """
         Assign the authenticated user as the author of the comment.
         """
         serializer.save(author=self.request.user)
+
