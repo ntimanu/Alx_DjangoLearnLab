bef60c8d93761ccbeb5ab743ab49ad3ee68448e0
ntimanu <ntihinyukae@gmail.com>
2025-03-31 15:05:24 +0200

    Fix: Added get_object_or_404 for like/unlike views

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -3,14 +3,13 @@ from .models import Post, Comment, Like
 from .serializers import PostSerializer, CommentSerializer
 from .permissions import IsOwnerOrReadOnly
 from rest_framework.response import Response
-from django.shortcuts import get_object_or_404
 from notifications.models import Notification
 from django.contrib.contenttypes.models import ContentType
 
 class FeedView(generics.ListAPIView):
     serializer_class = PostSerializer
     permission_classes = [permissions.IsAuthenticated]
-
+    
     def get_queryset(self):
         user = self.request.user
         following_users = user.following.all()
@@ -22,7 +21,7 @@ class PostViewSet(viewsets.ModelViewSet):
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
     filter_backends = [filters.SearchFilter]
     search_fields = ['title', 'content']
-
+    
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
@@ -30,21 +29,21 @@ class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
     permission_classes = [permissions.IsAuthenticated, IsOwnerOrReadOnly]
-
+    
     def get_queryset(self):
         post_id = self.kwargs.get('post_pk') 
         return super().get_queryset().filter(post_id=post_id).order_by('-created_at')
-
+    
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
 
 class LikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
-
+    
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like, created = Like.objects.get_or_create(user=request.user, post=post)
-
+        
         if created:
             Notification.objects.create(
                 recipient=post.author,
@@ -57,11 +56,11 @@ class LikePostView(generics.GenericAPIView):
 
 class UnlikePostView(generics.GenericAPIView):
     permission_classes = [permissions.IsAuthenticated]
-
+    
     def post(self, request, pk):
-        post = get_object_or_404(Post, pk=pk)
+        post = generics.get_object_or_404(Post, pk=pk)
         like = Like.objects.filter(user=request.user, post=post)
-
+        
         if like.exists():
             like.delete()
             return Response({"message": "Post unliked successfully."}, status=status.HTTP_200_OK)
